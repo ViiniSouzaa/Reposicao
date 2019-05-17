f3223969c67ac142938de4044c945abf7a129788
Vinicius Souza <viniciussilvasouza62@gmail.com>
2019-05-17 12:45:13 -0300

    Alteracao e inclusão do DTO

---
+++ src/main/java/com/mycompany/reposicao/ReposicaoApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.reposicao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposicaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposicaoApplication.class, args);
+	}
+
+}
