009e3ecc9a7d83836ceaee4aeda81d49ab6b2d42
Vinicius da Silva Souza <Vinicius@CPD4512.pmcp.pr.gov.br>
2019-07-11 14:49:55 -0300

    atualizacao 2

--- back-end/src/main/java/com/mycompany/reposicao/ReposicaoApplication.java
+++ back-end/src/main/java/com/mycompany/reposicao/ReposicaoApplication.java
@@ -1,13 +1,22 @@
 package com.mycompany.reposicao;
 
+import com.mycompany.dao.DocenteDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReposicaoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReposicaoApplication.class, args);
-	}
+    private final DocenteDAO docenteDAO;
+
+    @Autowired
+    public ReposicaoApplication(DocenteDAO docenteDAO) {
+        this.docenteDAO = docenteDAO;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReposicaoApplication.class, args);
+    }
 
 }
